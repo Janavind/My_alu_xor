06ba37894dd33862fc01427a071cfa3a2748016b
Janavind <jaravind@aggies.ncat.edu>
2022-03-14 19:45:18 -0400

    modified:   def/user_proj_example.def 	modified:   gds/user_proj_example.gds 	modified:   lef/user_proj_example.lef 	modified:   mag/user_proj_example.mag 	modified:   maglef/user_proj_example.mag 	modified:   openlane/user_project_wrapper/config.tcl 	modified:   signoff/user_proj_example/final_summary_report.csv 	modified:   spi/lvs/user_proj_example.spice 	modified:   verilog/gl/user_proj_example.v 	modified:   verilog/rtl/uprj_netlists.v 	modified:   verilog/rtl/user_proj_example.v 	modified:   verilog/rtl/user_project_wrapper.v 	openlane/user_project_wrapper/macros/ 	sdc/ 	sdf/ 	spef/

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -1,6 +1,6 @@
 `default_nettype none
 module user_proj_example(
-`ifdef USE_POER_PINS
+`ifdef USE_POWER_PINS
 	 inout vccd1,	// User area 1 1.8V supply
          inout vssd1,	// User area 1 digital ground
  `endif
